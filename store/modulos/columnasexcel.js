32a595c1c7110cb39a1679d8aaedab392325ca5a
Marco González <marcoignacio.9637@gmail.com>
2022-11-10 18:31:23 -0300

    fix:manejo de estados para columnas y filas de archivos

--- store/modulos/columnasexcel.js
+++ store/modulos/columnasexcel.js
@@ -1,15 +1,22 @@
 export const state = () => ({
-  row_columnas_funcionarios:1
+  row_columnas_funcionarios:1,
+  row_columnas_grupo_uno:1
 });
 
 export const mutations = {
   SET_COLUMNA_FUNCIONARIOS(state, row){
     state.row_columnas_funcionarios = row;
+  },
+  SET_COLUMNA_GRUPO_UNO(state, row){
+    state.row_columnas_grupo_uno = row;
   }
 };
 
 export const getters = {
   rowColumnasFuncionarios(state){
     return state.row_columnas_funcionarios;
+  },
+  rowColumnasGrupoUno(state){
+    return state.row_columnas_grupo_uno;
   }
 };
