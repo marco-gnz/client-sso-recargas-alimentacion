a0ee7660bd6c520b251874c7621705a9e62ff603
Marco González <marcoignacio.9637@gmail.com>
2022-11-10 16:59:19 -0300

    fix:Funcion para cambiar nombre de columnas de archivo, posición de columnas en archivo y agrupación de funciones depenediendo respuesta de API

--- components/admin/recargas/files/ausentismos.vue
+++ components/admin/recargas/files/ausentismos.vue
@@ -11,7 +11,7 @@
         <section class="modal-card-body p-6">
           <el-steps :active="paso" align-center>
             <el-step title="Paso 1" description="Grupo de ausentismo"></el-step>
-            <el-step title="Paso 2" description="Descripción de columnas "></el-step>
+            <el-step title="Paso 2" description="Descripción de columnas"></el-step>
             <el-step title="Paso 3" description="Seleccionar archivo"></el-step>
             <el-step title="Paso 4" description="Verificar datos"></el-step>
             <el-step title="Paso 5" description="Importación exitosa"></el-step>
@@ -64,10 +64,135 @@
             </div>
           </div>
         </section>
+        <section v-if="paso === 1" class="modal-card-body">
+          <h6>Lass siguientes columnas son compatibles para realizar la carga masiva de ausentismos para el grupo {{grupo_value}}.</h6>
+          <div class="columns">
+            <div class="column is-half">
+              <label class="label">Posición columnas (2 max)</label>
+              <input type="number" class="input is-rounded" v-model="row_grupo_uno">
+            </div>
+          </div>
+          <div class="table-container">
+            <table class="table is-fullwidth">
+              <thead>
+                <tr>
+                  <th>Nombre columna</th>
+                  <th>Formato</th>
+                  <th>Requerida</th>
+                  <th>Descripción</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="(columna, index) in columnas_grupo_uno" :key="index">
+                  <td><input type="text" class="input is-rounded" v-model="columna.nombre_columna" v-uppercase></td>
+                  <td>{{columna.formato}}</td>
+                  <td><el-tag :type="columna.required ? 'success' : 'warning'" disable-transitions>{{`${columna.required ? 'Si' : 'No'}`}}</el-tag></td>
+                  <td>{{columna.descripcion}}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </section>
+        <section v-if="paso === 2" class="modal-card-body">
+          <div class="columns">
+            <div class="column">
+              <div class="field">
+                <el-upload
+                  class="avatar-uploader"
+                  :action="`#`"
+                  :show-file-list="false"
+                  :on-success="handleAvatarSuccess"
+                  :before-upload="beforeAvatarUpload">
+                  <i class="el-icon-files"></i>
+                  <div v-if="!file_ausentismo" class="el-upload__text">Click para cargar archivo excel (Formato .XLSX)</div>
+                  <span v-else>{{this.file_ausentismo.name}}</span>
+                </el-upload>
+                <template v-if="ausentismos.length && !errors_file">
+                  <el-result icon="success" title="Archivo analizado correctamente" :subTitle="`${ausentismos.length} ${ausentismos.length > 1 ? `registros analizados` : `registros analizado`}`">
+                    <template slot="extra">
+                      <el-button type="danger" size="medium">Remover archivo</el-button>
+                    </template>
+                  </el-result>
+                </template>
+                <template v-if="errorColumn.status === 'Error'">
+                  <el-result icon="error" :title="errorColumn.message" subTitle="Favor verificar nuevamente el nombre de las columnas en el paso anterior.">
+                    <template slot="extra">
+                      <el-button type="danger" size="medium">Remover archivo</el-button>
+                    </template>
+                  </el-result>
+                </template>
+                <template v-if="errors_file != null">
+                    <el-result icon="error" title="Error al analizar archivo">
+                      <template slot="extra">
+                        <el-button type="danger" size="medium">Remover archivo</el-button>
+                        {{`${errors_file.length} ${errors_file.length > 1 ? `errores` : `error`}`}}
+                        <table class="table is-fullwidth">
+                          <thead>
+                            <tr>
+                              <th>N° de fila</th>
+                              <th>Atributo</th>
+                              <th>Error</th>
+                              <th>Valores</th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            <tr v-for="(e, index) in errors_file" :key="index">
+                              <th>{{e.row}}</th>
+                              <th>{{e.attribute}}</th>
+                              <th>{{e.errors.map(m => m).join(', ')}}</th>
+                              <th>{{e.values}}</th>
+                            </tr>
+                          </tbody>
+                        </table>
+                      </template>
+                    </el-result>
+                </template>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section v-if="paso === 3" class="modal-card-body">
+          <div class="columns">
+            <div class="column">
+              <template v-if="ausentismos.length">
+                <h4 class="title is-4">Listado de registros</h4>
+                <span class="tag is-primary is-light">{{`${ausentismos.length} ${ausentismos.length > 1 ? `registros` : `registro` }`}}</span>
+                <table class="table is-bordered is-striped is-narrow is-hoverable is-fullwidth">
+                  <thead>
+                    <tr>
+                      <th v-for="(fila, index) in filas" :key="index">{{index}}</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr v-for="(ausentismo, index) in ausentismos" :key="index">
+                      <th v-for="(a, index) in ausentismo" :key="index">{{a}}</th>
+                    </tr>
+                  </tbody>
+                </table>
+              </template>
+              <template v-if="!successImport">
+                <el-result icon="error" title="Error al cargar archivo">
+                  <template slot="extra">
+                    <span>Existe un error en el archivo. Favor volver y analizar nuevamente.</span>
+                  </template>
+                </el-result>
+              </template>
+            </div>
+          </div>
+        </section>
+        <section v-if="paso === 4 && successImport" class="modal-card-body">
+          <div class="columns">
+            <div class="column">
+              <el-result icon="success" title="Operación realizada con éxito" :subTitle="`${successMessagge}`">
+              </el-result>
+            </div>
+          </div>
+        </section>
         <footer class="modal-card-foot buttons is-right">
+          <button v-if="paso === 4 && successImport" @click.prevent="hideAusentismosModal" class="button is-rounded">Cerrar</button>
           <button :disabled="paso === 0" v-if="paso != 4" @click.prevent="volver" class="button is-rounded">Volver</button>
-          <button v-if="paso < 2" @click.prevent="siguiente" v-loading.fullscreen.lock="loadingSpinner" class="button is-info is-rounded">Siguiente</button>
-          <button v-if="paso === 2" class="button is-primary is-rounded" v-loading.fullscreen.lock="loadingSpinner">Cargar datos</button>
+          <button v-if="paso < 3" :disabled="disabledButton" @click.prevent="siguiente" v-loading.fullscreen.lock="loadingSpinner" class="button is-info is-rounded">Siguiente</button>
+          <button :disabled="!successImport" v-if="paso === 3" class="button is-primary is-rounded" @click.prevent="storeGrupoUno" v-loading.fullscreen.lock="loadingSpinner">Cargar datos</button>
         </footer>
       </div>
     </div>
@@ -80,8 +205,14 @@ export default {
   props:['codigo'],
   data(){
     return{
-      radio1:''
+      radio1:'',
+      grupo_select:[],
+      columnas_grupo_uno:[]
+
     };
+  },
+  mounted(){
+    this.getColumnsGrupoUno();
   },
   computed:{
     ...mapGetters({
@@ -91,7 +222,20 @@ export default {
       grupos:'modulos/modulos/gruposAusentismos',
       reglas:'recargas/ausentismos/reglas',
       loadingReglas:'recargas/ausentismos/loadingReglas',
+      ausentismos:'recargas/ausentismos/ausentismos',
+      filas: "recargas/ausentismos/filas",
+      successImport: "recargas/ausentismos/successImport",
+      successMessagge: "recargas/ausentismos/successMessagge",
+      errorColumn: "recargas/ausentismos/errorsColumn",
     }),
+    row_grupo_uno:{
+        get() {
+          return this.$store.state.modulos.columnasexcel.row_columnas_grupo_uno;
+        },
+        set(newValue) {
+          this.$store.commit('modulos/columnasexcel/SET_COLUMNA_GRUPO_UNO', newValue);
+        }
+      },
     grupo_value:{
       get() {
         return this.$store.state.recargas.ausentismos.carga.grupo_id;
@@ -100,18 +244,99 @@ export default {
         this.$store.commit('recargas/ausentismos/SET_GRUPO', newValue);
       }
     },
+    file_ausentismo:{
+      get() {
+        return this.$store.state.recargas.ausentismos.carga.file;
+      },
+      set(newValue) {
+        this.$store.commit('recargas/ausentismos/SET_FILE', newValue);
+      }
+    },
+    errors_file:{
+      get() {
+        return this.$store.state.recargas.ausentismos.errors_file;
+      },
+      set(newValue) {
+        this.$store.commit('recargas/ausentismos/SET_ERRORS_FILE', newValue);
+      }
+    },
+    disabledButton(){
+      let value = false;
+
+      if(this.paso === 0 && !this.grupo_value){
+        value = true;
+      }else if(this.paso === 2 && !this.file_ausentismo){
+        value = true;
+      }else if((this.paso === 2 && this.errors_file) || (this.paso === 2 && this.errorColumn)){
+        value = true;
+      }
+
+      return value;
+    }
   },
   methods:{
     ...mapActions({
       closeModal:'recargas/ausentismos/closeModal',
-      getReglasGrupoAction:'recargas/ausentismos/getReglas'
+      getReglasGrupoAction:'recargas/ausentismos/getReglas',
+      loadFileAction:'recargas/ausentismos/uploadFileAusentismo',
+      storeFileGrupoUnoAction:'recargas/ausentismos/storeFileGrupoUno',
     }),
+    async getColumnsGrupoUno(){
+      const url       = '/api/admin/modulos/columnas/grupo-uno';
+      const response  = await this.$axios.$get(url);
+      this.columnas_grupo_uno   = response;
+    },
     getReglasGrupo:function(){
       const data = {
         grupo_id:this.grupo_value,
         recarga_codigo:this.codigo
       };
+      if(this.grupo_value === 1){
+        this.grupo_select = this.columnas_grupo_uno;
+      }else if (this.grupo_value === 2){
+        this.grupo_select = this.columnas_grupo_dos;
+      }else{
+        this.grupo_select = this.columnas_grupo_tres;
+      }
       this.getReglasGrupoAction(data);
+    },
+    uploadFileHtml:function(){
+      if(this.file_ausentismo){
+        const data = {
+          recarga_codigo:this.codigo,
+          grupo_id:this.grupo_value,
+          file:this.file_ausentismo,
+          columnas:this.columnas_grupo_uno,
+          row_columnas:this.row_grupo_uno
+        };
+        this.loadFileAction(data);
+      }else{
+        this.$message.error('Por favor seleccione archivo');
+      }
+    },
+    handleAvatarSuccess(res, file) {
+      this.file_ausentismo = file.raw;
+      this.imageUrl = URL.createObjectURL(file.raw);
+      if(this.file_ausentismo){
+        this.uploadFileHtml();
+      }
+    },
+    beforeAvatarUpload(file) {
+      const isXLSX = file.type === 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet';
+      if (!isXLSX) {
+        this.$message.error('El archivo debe ser formato .XLSX');
+      }
+      return isXLSX;
+    },
+    storeGrupoUno:function(){
+      const data = {
+          recarga_codigo:this.codigo,
+          grupo_id:this.grupo_value,
+          file:this.file_ausentismo,
+          columnas:this.columnas_grupo_uno,
+          row_columnas:this.row_grupo_uno
+        };
+      this.storeFileGrupoUnoAction(data);
     },
     hideAusentismosModal:function(){
       this.closeModal();
--- store/recargas/ausentismos.js
+++ store/recargas/ausentismos.js
@@ -7,13 +7,21 @@ export const state = () => ({
     grupo_id:'',
     file:''
   },
-  errors:{},
-  reglas:[]
+  errors_file:null,
+  reglas:[],
+  ausentismos:[],
+  filas:[],
+  success_import:false,
+  message_success:'',
+  errors_column:''
 });
 
 export const mutations = {
   SET_MODAL(state, value){
     state.open_modal = value;
+  },
+  SET_LOADING(state, value){
+    state.full_screen_loading = value;
   },
   SET_NEGATIVE_PASO_MODAL(state){
     state.paso--;
@@ -21,14 +29,38 @@ export const mutations = {
   SET_POSITIVE_PASO_MODAL(state){
     state.paso++;
   },
+  SET_POSITION_PASO_MODAL(state, value){
+    state.paso = value;
+  },
   SET_REGLAS(state, value){
     state.reglas = value;
   },
   SET_GRUPO(state, value){
     state.carga.grupo_id = value;
   },
+  SET_FILE(state, value){
+    state.carga.file = value;
+  },
   SET_LOADING_REGLAS(state, value){
     state.loading_reglas = value;
+  },
+  SET_ERRORS_FILE(state, value){
+    state.errors_file = value;
+  },
+  SET_AUSENTISMOS(state, value){
+    state.ausentismos = value;
+  },
+  SET_FILAS(state, value){
+    state.filas = value;
+  },
+  SET_SUCCESS_IMPORT(state, value){
+    state.success_import = value;
+  },
+  SET_SUCCESS_MESSAGE_IMPORT(state, value){
+    state.message_success = value;
+  },
+  SET_ERROR_COLUMN(state, value){
+    state.errors_column = value;
   }
 };
 
@@ -48,26 +80,124 @@ export const getters = {
   loadingReglas(state){
     return state.loading_reglas;
   },
+  ausentismos(state){
+    return state.ausentismos;
+  },
+  filas(state){
+    return state.filas;
+  },
+  successImport(state){
+    return state.success_import;
+  },
+  successMessagge(state){
+    return state.message_success;
+  },
+  errorsColumn(state){
+    return state.errors_column;
+  }
 };
 
 export const actions = {
+  successLoadFile({ commit }){
+    commit('SET_AUSENTISMOS', []);
+    commit('SET_ERRORS_FILE', null);
+    commit('SET_ERROR_COLUMN', '');
+    commit('SET_SUCCESS_IMPORT', true);
+  },
+  errorsLoadFile({ commit }){
+    commit('SET_FILE', '');
+    commit('SET_GRUPO', '');
+    commit('SET_AUSENTISMOS', []);
+    commit('SET_FILAS', []);
+  },
+  successStoreFile({ commit }){
+    commit('SET_FILE', '');
+    commit('SET_GRUPO', '');
+    commit('SET_REGLAS', []);
+    commit('SET_GRUPO', '');
+    commit('SET_AUSENTISMOS', []);
+  },
   closeModal({ commit }){
     commit('SET_MODAL', false);
+    commit('SET_POSITION_PASO_MODAL', 0);
   },
   async getReglas({ commit }, data){
     commit('SET_LOADING_REGLAS', true);
-    console.log(data);
     const url = `/api/admin/recargas/grupo/reglas`;
 
     await this.$axios.$get(url, {params:data}).then(response => {
       commit('SET_LOADING_REGLAS', false);
-      console.log(response);
       if(response.status === 'Success'){
         commit('SET_REGLAS', response.data)
       }
     }).catch(error => {
       commit('SET_LOADING_REGLAS', false);
       console.log(error);
+    });
+  },
+  async uploadFileAusentismo({ commit, dispatch }, data){
+    commit('SET_LOADING', true);
+    let formData = new FormData();
+    formData.append('codigo', data.recarga_codigo);
+    formData.append('grupo_id', data.grupo_id);
+    formData.append('file', data.file);
+    formData.append('columnas', JSON.stringify(data.columnas));
+    formData.append('row_columnas', data.row_columnas);
+    const url = '/api/admin/recargas/recarga/masivo/grupo/uno';
+
+    await this.$axios.$post(url, formData, {
+      headers:{
+        'Content-Type': 'multipart/form-data'
+      }
+    }).then(response => {
+      commit('SET_LOADING', false);
+      if(response.status === 'Success'){
+        dispatch('successLoadFile');
+        commit('SET_FILAS', response.data[0]);
+        commit('SET_AUSENTISMOS', response.data);
+      }else{
+        dispatch('errorsLoadFile');
+        commit('SET_ERRORS_FILE', response[1]);
+      }
+    }).catch(error => {
+      dispatch('errorsLoadFile');
+      commit('SET_LOADING', false);
+      commit('SET_ERRORS_FILE', error[1]);
+      commit('SET_ERROR_COLUMN', error.response.data);
+      console.log(error);
+    });
+  },
+  async storeFileGrupoUno({ commit, dispatch }, data){
+    commit('SET_LOADING', true);
+    const url = '/api/admin/recargas/recarga/masivo/grupo/uno/import';
+
+    let formData = new FormData();
+    formData.append('codigo', data.recarga_codigo);
+    formData.append('grupo_id', data.grupo_id);
+    formData.append('file', data.file);
+    formData.append('columnas', JSON.stringify(data.columnas));
+    formData.append('row_columnas', data.row_columnas);
+    await this.$axios.$post(url, formData, {
+      headers:{
+        'Content-Type': 'multipart/form-data'
+      }
+    }).then(response => {
+      commit('SET_LOADING', false);
+      if(response.status === 'Success'){
+        dispatch('successStoreFile');
+        commit('SET_POSITION_PASO_MODAL', 4);
+        commit('SET_SUCCESS_MESSAGE_IMPORT', response.message);
+      }else{
+        dispatch('errorsLoadFile');
+        commit('SET_ERRORS_FILE', response[1]);
+        commit('SET_SUCCESS_IMPORT', false);
+      }
+    }).catch(error => {
+      console.log(error);
+      dispatch('errorsLoadFile');
+      commit('SET_LOADING', false);
+      commit('SET_ERRORS_FILE', error[1]);
+      commit('SET_SUCCESS_IMPORT', false);
     });
   }
 };
